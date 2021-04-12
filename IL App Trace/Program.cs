dd132a8a9ce3f8494ab17f93aa9fbfdb1180f36d
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2021-04-12 22:56:11 +0200

    Projektdateien hinzufügen.

---
+++ IL App Trace/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Mono.Cecil;
+using System.IO;
+using Mono.Cecil.Cil;
+using System.Linq;
+
+namespace IL_App_Trace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sharkbyteprojects Assembly Tracer\n");
+            if (args.Length > 0)
+            {
+                foreach (string filename in args)
+                {
+                    if (File.Exists(filename))
+                    {
+                        Console.WriteLine($"Modify \"{filename}\"");
+                        ModuleDefinition module = ModuleDefinition.ReadModule(filename);
+                        if (module.Kind == ModuleKind.Windows)
+                            module.Kind = ModuleKind.Console;
+                        MethodReference clog = module.ImportReference(typeof(Console).GetMethod("WriteLine", new Type[] { typeof(object) }));
+                        Instruction loginst = Instruction.Create(OpCodes.Call, clog);
+                        foreach (var type in module.Types)
+                        {
+                            foreach (var method in type.Methods)
+                            {
+                                if (!method.HasBody) continue;
+                                var ilProc = method.Body.GetILProcessor();
+                                var fi = method.Body.Instructions.First();
+                                var li = method.Body.Instructions.Last();
+                                ilProc.InsertBefore(fi, Instruction.Create(OpCodes.Ldstr, $"-- Entering {method.Name} of {type.Name}"));
+                                ilProc.InsertBefore(fi, loginst);
+                                ilProc.InsertBefore(li, Instruction.Create(OpCodes.Ldstr, $"-- Exiting {method.Name} of {type.Name}"));
+                                ilProc.InsertBefore(li, loginst);
+                            }
+                        }
+                        string saveas = string.Format("{0}.trace{1}", Path.GetFileNameWithoutExtension(filename), Path.GetExtension(filename));
+                        Console.WriteLine($"Save File: {saveas}");
+                        module.Write(saveas);
+                    }
+                }
+                Console.WriteLine("Completed");
+            }
+            else
+            {
+                Console.WriteLine("Give as Args IL Assembly Files!");
+                Console.Beep();
+            }
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TestFunktion();
+        }
+        static void TestFunktion()
+        {
+            Console.WriteLine("Tue Etwas und warte auf Benutzereingabe...");
+            Console.ReadKey();
+        }
+    }
+}
